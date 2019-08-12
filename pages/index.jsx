1bb34c9d4fd702e911713e8fbb2b2c0ba65bd415
kozlovvski <mcbiggos@gmail.com>
2019-08-12 22:30:03 +0200

    remove obsolete imports

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,59 +1,39 @@
 import Head from "../components/Head";
 import Layout from "../components/Layout";
-import {
-  Grid,
-  Paper,
-  Collapse,
-  Box,
-  IconButton,
-  ButtonBase
-} from "@material-ui/core";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { Grid, Paper } from "@material-ui/core";
 import DriversStandings from "../components/DriversStandings";
-import { ChevronDown, ChevronUp } from "mdi-material-ui";
 import axios from "axios";
-import { useState } from "react";
 import CollapseWithButton from "../components/ui/CollapseWithButton";
 import ConstructorsStandings from "../components/ConstructorsStandings";
 
-const useStyles = makeStyles(theme => ({}));
-
-const Home = props => {
-  const classes = useStyles();
-
-  return (
-    <Layout name="Dashboard">
-      <Head title="Home" />
-      <Grid container spacing={3}>
-        <Grid item xs={12} lg={4}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={6} lg={12}>
-              <Paper className={classes.paper}>
-                <CollapseWithButton height="200px">
-                  <DriversStandings
-                    data={props.driversStandingsData}
-                  />
-                </CollapseWithButton>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={6} lg={12}>
-              <Paper className={classes.paper}>
-                <CollapseWithButton height="200px">
-                  <ConstructorsStandings
-                    data={props.constructorsStandingsData}
-                  />
-                </CollapseWithButton>
-              </Paper>
-            </Grid>
+const Home = props => (
+  <Layout name="Dashboard">
+    <Head title="Home" />
+    <Grid container spacing={3}>
+      <Grid item xs={12} lg={4}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} lg={12}>
+            <Paper>
+              <CollapseWithButton height="200px">
+                <DriversStandings data={props.driversStandingsData} />
+              </CollapseWithButton>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} lg={12}>
+            <Paper>
+              <CollapseWithButton height="200px">
+                <ConstructorsStandings data={props.constructorsStandingsData} />
+              </CollapseWithButton>
+            </Paper>
           </Grid>
         </Grid>
-        <Grid item xs={12} lg={8}>
-          <Paper className={classes.paper}>elo2</Paper>
-        </Grid>
       </Grid>
-    </Layout>
-  );
-};
+      <Grid item xs={12} lg={8}>
+        <Paper>elo2</Paper>
+      </Grid>
+    </Grid>
+  </Layout>
+);
 
 Home.getInitialProps = async () => {
   const getDriversStandings = async () => {
@@ -72,9 +52,9 @@ Home.getInitialProps = async () => {
     );
     const resData = await res.data;
     const constructorsStandings = await resData.MRData.StandingsTable
-    .StandingsLists[0];
+      .StandingsLists[0];
     return constructorsStandings;
-  }
+  };
 
   return {
     driversStandingsData: await getDriversStandings(),
