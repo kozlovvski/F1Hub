5c9b78bf9218c889dfe2955ce438d5da97078518
kozlovvski <mcbiggos@gmail.com>
2019-08-23 00:26:58 +0200

    fix broken layout in PositionDifference

--- components/ui/PositionDifference.jsx
+++ components/ui/PositionDifference.jsx
@@ -1,16 +1,14 @@
-import { Typography } from "@material-ui/core";
-
 const PositionDifference = props => {
 	const { initPos, endPos } = props;
 	const posDiff = initPos - endPos;
 
 	return posDiff > 0 ? (
-		<Typography variant="caption" style={{ color: "#15b33f" }}>{` (+${posDiff})`}</Typography>
+		<span style={{ color: "#15b33f", fontSize: "0.85em"}}>{` (+${posDiff})`}</span>
 	) : (
 		(posDiff == 0 ? (
-			<Typography variant="caption" style={{ color: "#dba100" }}>{` (0)`}</Typography>
+			<span style={{ color: "#dba100", fontSize: "0.85em"}}>{` (0)`}</span>
 		) : (
-			<Typography variant="caption" style={{ color: "#bf0f0f" }}>{` (${posDiff})`}</Typography>
+			<span style={{ color: "#bf0f0f", fontSize: "0.85em"}}>{` (${posDiff})`}</span>
 		))
 	);
 };
